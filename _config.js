df97ab346e74ba3882a500d7b136baaf69612133
Morgan-Gicheha <morgangicheha4@gmail.com>
2024-09-22 00:32:41 +0300

    debug mongo connections tring

--- _config.js
+++ _config.js
@@ -2,9 +2,9 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://gicheha:gicheha@cluster0.xuqdg.mongodb.net/darkroom/?retryWrites=true&w=majority',
-    development: 'mongodb+srv://gicheha:gicheha@cluster0.xuqdg.mongodb.net/darkroom-dev/?retryWrites=true&w=majority',
-    test: 'mongodb+srv://gicheha:gicheha@cluster0.xuqdg.mongodb.net/darkroom-test/?retryWrites=true&w=majority',
+    production: 'mongodb+srv://gicheha:gicheha@cluster0.xuqdg.mongodb.net/darkroom?retryWrites=true&w=majority',
+    development: 'mongodb+srv://gicheha:gicheha@cluster0.xuqdg.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+    test: 'mongodb+srv://gicheha:gicheha@cluster0.xuqdg.mongodb.net/darkroom-test?retryWrites=true&w=majority',
 }
 
 module.exports = config;
